ad443fa2f5daccd7b61944dc374eb83fdd614ae7
RomanoK <romanokeser@gmail.com>
2023-01-21 14:31:08 +0100

    Implemented the OpenXml library, reading the Word document and writing its data to text file

--- WordContentReader/WordContentReader/Program.cs
+++ WordContentReader/WordContentReader/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Specify the path of the Word document
+        string wordFile = @"C:/Users/Romano/Desktop/TestWord.docx";
+
+        // Open the Word document
+        using (WordprocessingDocument doc = WordprocessingDocument.Open(wordFile, false))
+        {
+            // Get the main document part
+            MainDocumentPart mainPart = doc.MainDocumentPart;
+
+            // Get the text of the document
+            string text = mainPart.Document.InnerText;
+
+            // Specify the path of the text file
+            string textFile = @"C:/Users/Romano/Desktop/TestOutput.txt";
+
+            // Write the text to the file
+            File.WriteAllText(textFile, text);
+        }
+    }
+}
