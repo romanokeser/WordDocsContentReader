9b2ef0e4a8f3bc2dcee33212ff03238e9fa68792
RomanoK <romanokeser@gmail.com>
2023-01-21 15:19:05 +0100

    Correct data export, included comma separator

--- WordContentReader/WordContentReader/Program.cs
+++ WordContentReader/WordContentReader/Program.cs
@@ -7,9 +7,124 @@ class Program
 {
     static void Main(string[] args)
     {
+        //ReadOnlyTableContentOnSteroids();
+        ReadTableAndWriteContentOnSteroids();
         //ReadOnlyTableContent();
         //ReadEachTableContent();
         //ReadAndWriteWordDataToTextfile()
+    }
+
+    /// <summary>
+    /// Check if the current cell is the last one in the row or not
+    /// If it's not the last one it will append a comma to separate the content of the cell to the next one
+    /// If it's the last one it will append only the text
+    /// </summary>
+    static void ReadTableAndWriteContentOnSteroids()
+    {
+        // Specify the path of the Word document
+        string wordFile = @"C:/Users/Romano/Desktop/TestWord.docx";
+
+        // Open the Word document
+        using (WordprocessingDocument doc = WordprocessingDocument.Open(wordFile, false))
+        {
+            // Get the main document part
+            MainDocumentPart mainPart = doc.MainDocumentPart;
+            var tables = mainPart.Document.Body.Elements<Table>();
+
+            // Specify the path of the text file
+            string textFile = @"C:\Users\Romano\Desktop\TestOutput.txt";
+
+            // Iterate through each table
+            for (int i = 0; i < tables.Count(); i++)
+            {
+                var rows = tables.ElementAt(i).Elements<TableRow>();
+
+                // Iterate through each row
+                for (int j = 0; j < rows.Count(); j++)
+                {
+                    var cells = rows.ElementAt(j).Elements<TableCell>();
+
+                    // Iterate through each cell
+                    for (int k = 0; k < cells.Count(); k++)
+                    {
+                        var paragraphs = cells.ElementAt(k).Elements<Paragraph>();
+                        // Iterate through each paragraph
+                        for (int l = 0; l < paragraphs.Count(); l++)
+                        {
+                            var runs = paragraphs.ElementAt(l).Elements<Run>();
+
+                            // Iterate through each run
+                            for (int m = 0; m < runs.Count(); m++)
+                            {
+                                // Get the text of the run
+                                var text = runs.ElementAt(m).InnerText;
+                                // Write the text to the file, followed by a comma if it's not the last cell in the row
+                                if (k != cells.Count() - 1)
+                                {
+                                    File.AppendAllText(textFile, text + ", ");
+                                }
+                                else
+                                {
+                                    File.AppendAllText(textFile, text);
+                                }
+                            }
+                        }
+                    }
+                    // Write a new line after each row
+                    File.AppendAllText(textFile, "\r\n");
+                }
+                // Write a new line after each table
+                File.AppendAllText(textFile, "\r\n");
+            }
+        }
+    }
+
+    static void ReadOnlyTableContentOnSteroids()
+    {
+        // Specify the path of the Word document
+        string wordFile = @"C:/Users/Romano/Desktop/TestWord.docx";
+
+        // Open the Word document
+        using (WordprocessingDocument doc = WordprocessingDocument.Open(wordFile, false))
+        {
+            // Get the main document part
+            MainDocumentPart mainPart = doc.MainDocumentPart;
+            var tables = mainPart.Document.Body.Elements<Table>();
+
+            // Specify the path of the text file
+            string textFile = @"C:\Users\Romano\Desktop\TestOutput.txt";
+
+            // Iterate through each table
+            for (int i = 0; i < tables.Count(); i++)
+            {
+                var rows = tables.ElementAt(i).Elements<TableRow>();
+
+                // Iterate through each row
+                for (int j = 0; j < rows.Count(); j++)
+                {
+                    var cells = rows.ElementAt(j).Elements<TableCell>();
+
+                    // Iterate through each cell
+                    for (int k = 0; k < cells.Count(); k++)
+                    {
+                        var paragraphs = cells.ElementAt(k).Elements<Paragraph>();
+                        // Iterate through each paragraph
+                        for (int l = 0; l < paragraphs.Count(); l++)
+                        {
+                            var runs = paragraphs.ElementAt(l).Elements<Run>();
+
+                            // Iterate through each run
+                            for (int m = 0; m < runs.Count(); m++)
+                            {
+                                // Get the text of the run
+                                var text = runs.ElementAt(m).InnerText;
+                                File.AppendAllText(textFile, text + "\r\n");
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /// <summary>
